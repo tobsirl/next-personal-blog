f6bd6d207e116718ce679111ec05ece43a242bec
Paul Tobin <tobster111@gmail.com>
2023-02-12 10:21:32 +0000

    Next Personal Blog Link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,8 @@ export default function Contact() {
   return (
     <div>
       <h1>contact</h1>
-      <Link href="/blog">
-        <a>Blog</a>
+      <Link href="/">
+        Home
       </Link>
     </div>
   )
