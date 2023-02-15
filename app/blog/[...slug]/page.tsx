3e26a8b57ab4c2e863b9b698f07094d673614ec1
Paul Tobin <tobster111@gmail.com>
2023-02-15 19:09:43 +0000

    Next-Personal-Blog Params

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,12 @@
-export default function BlogPost({params}) {
+const getPost = async(slug) => {
+  const post = await fetch(`https://www.reddit.com/r/${slug}.json`)
+  return post.json()
+  
+}
+
+export default async function BlogPost({params}) {
   const { slug } = params
+  const post = await getPost(slug)
   return (
     <div>post</div>
   )
