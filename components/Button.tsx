9ef862ae35521c5cbbeee3588538f01447f88d7b
Paul Tobin <tobster111@gmail.com>
2023-02-17 12:21:46 +0000

    Next-Personal-Blog Styling

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+interface Button {
+  variant: string
+}
+
+
+const Button:  FC<Button> = ({ variant }) => {
+  return (
+    <button className={variant}>
+      Click me
+    </button>
+  )
+}
