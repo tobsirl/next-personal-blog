6f7ce5330a7adf27428e27a9fc490e16e59c8844
Paul Tobin <tobster111@gmail.com>
2023-02-18 09:03:40 +0000

    Next-Personal-Blog Styling

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.css'
+
+export default async function Home() {
+  return (
+    <div className={styles.home}>
+      <div>name</div>
+      <div>blog</div>
+      <div>contact us</div>
+    </div>
+  )
+}
