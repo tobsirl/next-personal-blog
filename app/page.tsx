938c3ea57d4e50a79eef2c3ed8597d185c36ede5
Paul Tobin <tobster111@gmail.com>
2023-02-18 09:12:24 +0000

    Next-Personal-Blog Global styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import styles from '../styles/home.module.css'
 export default async function Home() {
   return (
     <div className={styles.home}>
-      <div>name</div>
-      <div>blog</div>
-      <div>contact us</div>
+      <div>
+        <h1>Hi, my name is Paul Tobin</h1>
+        <div>blog</div>
+        <div>contact us</div>
+      </div>
     </div>
   )
 }
