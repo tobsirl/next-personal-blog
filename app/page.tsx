d1fd2b71f0bd39530ddcccb12586492d44217dff
Paul Tobin <tobster111@gmail.com>
2023-02-18 09:15:12 +0000

    Next-Personal-Blog Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from '../styles/home.module.css'
 
 export default async function Home() {
@@ -5,8 +6,8 @@ export default async function Home() {
     <div className={styles.home}>
       <div>
         <h1>Hi, my name is Paul Tobin</h1>
-        <div>blog</div>
-        <div>contact us</div>
+        <div><Link href="/blog">Checkout my Blog</Link></div>
+        <div><Link href="/contact">Contact Me</Link></div>
       </div>
     </div>
   )
